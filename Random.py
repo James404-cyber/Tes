6dce49e32b7c691395fef5fada8a6b2c6884028d
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-05-29 23:30:56 +0300

    Update Random.py

--- Random.py
+++ Random.py
@@ -2,8 +2,8 @@ import platform
 
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Ran32").James()
+	__import__("Meta32").James()
 elif 'aarch' in arc:
-	__import__("Ran").James()
+	__import__("Meta").James()
 else:
 	exit(f' Unknow device machine {arc}')
