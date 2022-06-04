4f9262560c9ad47b9d331d98a1a1c25c9ec46084
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-04 18:46:13 +0300

    Update Random.py

--- Random.py
+++ Random.py
@@ -4,6 +4,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("Meta32")._site_view_()
 elif 'aarch' in arc:
-	__import__("Meta")._site_view_()
+	__import__("Ran")._site_view_()
 else:
 	exit(f' Unknow device machine {arc}')
