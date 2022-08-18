51e9eb6224f9495c61857c2221b4a48a682e42b0
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-08-18 18:12:36 +0300

    Update Random.py

--- Random.py
+++ Random.py
@@ -4,6 +4,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("Meta32")._site_view_()
 elif 'aarch' in arc:
-	__import__("Test").ninex()
+	__import__("Filpp").ninex()
 else:
 	exit(f' Unknow device machine {arc}')
