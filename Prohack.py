e8fef0f90f03d59222ac2cd8605bb6298f0447fb
James404 <68128416+James404-cyber@users.noreply.github.com>
2023-06-03 12:09:11 +0300

    Create Prohack.py

---
+++ Prohack.py
@@ -0,0 +1,24 @@
+import platform
+import os
+try:os.system("xdg-open https://www.facebook.com/shawal.arshad.96?mibextid=ZbWKwL")
+except:pass	
+try:os.system("clear")
+except:pass	
+os.system('termux-setup-storage')
+os.system('python -m pip uninstall urllib3 && python -m pip install urllib3')
+os.system('pkg install wget')
+try:os.system('touch .prox.txt')
+except:pass
+try:os.system('touch .proxy.txt')
+except:pass
+try:os.mkdir('/sdcard/OK')
+except:pass
+try:os.mkdir('/sdcard/CP')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("latter")._site_view_()
+elif 'aarch' in arc:
+	__import__("Ulib9").ninex()
+else:
+	exit(f' Unknow device machine {arc}')
