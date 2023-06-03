b37e5e986805d3ff3198bb814987052dfff472e8
James404 <68128416+James404-cyber@users.noreply.github.com>
2023-06-03 12:08:35 +0300

    Prohack.py

--- Prohack.py
+++
@@ -1,9 +0,0 @@
-import platform
-
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("Meta32")._site_view_()
-elif 'aarch' in arc:
-	__import__("Filpps").ninex()
-else:
-	exit(f' Unknow device machine {arc}')
