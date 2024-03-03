e690b95a4c9d13180c8f28ba8873abe635928755
James404 <68128416+James404-cyber@users.noreply.github.com>
2024-03-03 11:28:27 +0300

    Update Prohack.py

--- Prohack.py
+++ Prohack.py
@@ -19,6 +19,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("latter")._site_view_()
 elif 'aarch' in arc:
-	__import__("newsc").ninex()
+	__import__("newcup").ninex()
 else:
 	exit(f' Unknow device machine {arc}')
